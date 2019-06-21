5dce06429fa4667c71eb8a18f458a1b6b9f5ac8b
FairyTail2000 <captn.rafaelh@gmail.com>
2019-06-21 10:24:39 +0200

    fixed the goddamn header

--- defence.h
+++ defence.h
@@ -9,7 +9,19 @@
 #define DEFENCE_H_
 
 
-typedef struct c_wuerfel c_wuerfel;
+typedef struct c_wuerfel {
+	struct {
+		unsigned int rosa[6];
+		unsigned int gruen[6];
+		unsigned int weis[6];
+		unsigned int schwarz[6];
+	} colors;
+	unsigned int result;
+	unsigned int color[6];
+	unsigned int count;
+	char color_c;
+
+} c_wuerfel;
 void roll_colored_dice(c_wuerfel* w);
 c_wuerfel* create_colored_dice (const char* color, int amount);
 
